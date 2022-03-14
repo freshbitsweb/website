700cce1528306f9268834579f3700a5eeccbe37c
misusonu18 <misusonu18@gmail.com>
2022-03-14 19:32:31 +0530

    code: fetching the blogs from the hashnode api

---
+++ functions/fetch_blogs.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+exports.handler = async function (event, context) {
+    try {
+        var data = {
+            query: `
+                query GetUserArticles {
+                    user(username: "freshbitsweb") {
+                        publication {
+                            posts(page: 0) {
+                                title
+                                brief
+                                slug
+                                coverImage
+                            }
+                    }
+                }
+            }
+        `,
+        };
+
+        var config = {
+            method: 'post',
+            url: 'https://api.hashnode.com/',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: await axios(config).then((res) => {
+                    return res.data.data;
+                }).catch((err) => {
+                    return err;
+                }),
+            }),
+        };
+    } catch (err) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({
+                msg: err.message,
+            }),
+        };
+    }
+};
