124b158f795753582134afb253cd9aa109540960
misusonu18 <misusonu18@gmail.com>
2023-01-19 09:39:57 +0530

    update the title name

--- js/diaries-data.js
+++ js/diaries-data.js
@@ -1093,11 +1093,11 @@ var diariesData = [
         "title": "Fun",
         "topic": "Dinner With Team Members at Rajan's new flat",
         "date": "1st January 2022",
-        "id": "dinner-with-team-members-at-rajan-s-new-flat-2023",
+        "id": "dinner-with-team-members-at-rajans-new-flat-2023",
         "images": [
             {
-                "media": "/images/freshbits-diaries/dinner-with-team-members-at-rajan-s-new-flat-2023/1.jpeg",
-                "thumbnail": "/images/freshbits-diaries/dinner-with-team-members-at-rajan-s-new-flat-2023/thumbnails/1.jpeg"
+                "media": "/images/freshbits-diaries/dinner-with-team-members-at-rajans-new-flat-2023/1.jpeg",
+                "thumbnail": "/images/freshbits-diaries/dinner-with-team-members-at-rajans-new-flat-2023/thumbnails/1.jpeg"
             },
         ],
         "tag": "fun"
